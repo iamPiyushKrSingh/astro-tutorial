a580fb8c0c340c5fda330f52a7cedc0f046fe67c
Piyush Kumar Singh <pksx9120@gmail.com>
2025-06-22 13:46:08 +0530

    changed site name

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-tutorial-pks.netlify.app",
+  site: "https://astro-pks.netlify.app",
 });
